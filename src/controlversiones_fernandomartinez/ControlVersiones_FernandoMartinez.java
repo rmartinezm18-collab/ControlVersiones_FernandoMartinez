128193c5926eb00fd18e8be170df6ce500eb5eee
Fernando Martinez <rmartinezm18@miumg.edu.gt>
2025-11-04 16:57:47 -0600

    Agregada función calcularPromedio y uso de arreglo

--- src/controlversiones_fernandomartinez/ControlVersiones_FernandoMartinez.java
+++ src/controlversiones_fernandomartinez/ControlVersiones_FernandoMartinez.java
@@ -4,28 +4,6 @@ import java.util.Scanner;
 
 public class ControlVersiones_FernandoMartinez {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("=== Calculadora de Promedio ===");
-
-        System.out.print("Ingrese la cantidad de numeros: ");
-        int n = sc.nextInt();
-
-        if (n <= 0) {
-            System.out.println("Cantidad invalida. El programa termina.");
-            return;
-        }
-
-        double[] numeros = new double[n];
-        for (int i = 0; i < n; i++) {
-            System.out.print("Numero " + (i + 1) + ": ");
-            numeros[i] = sc.nextDouble();
-        }
-
-        double promedio = calcularPromedio(numeros);
-        System.out.println("El promedio es: " + promedio);
-    }
-
     // Función para calcular promedio
     public static double calcularPromedio(double[] numeros) {
         double suma = 0;
@@ -33,5 +11,27 @@ public class ControlVersiones_FernandoMartinez {
             suma += n;
         }
         return suma / numeros.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== Calculadora de Promedio - Versión 2 ===");
+
+        System.out.print("Ingrese la cantidad de números: ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Cantidad inválida. El programa termina.");
+            return;
+        }
+
+        double[] numeros = new double[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = sc.nextDouble();
+        }
+
+        double promedio = calcularPromedio(numeros);
+        System.out.println("El promedio es: " + promedio);
     }
 }
